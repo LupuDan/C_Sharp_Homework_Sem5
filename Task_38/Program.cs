c55449735f0466acaaf9f71efdbfc57ee7be62f8
Lupu Daniel <dlupu666@gmail.com>
2023-04-08 18:11:52 +0300

    Task_38

---
+++ Task_38/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] arr = new double[10] {3.4, 1.2, 5.6, 2.1, 4.9, 7.8, 0.5, 2.3, 6.7, 1.8};
+
+        System.Console.WriteLine("Массив: ");
+        foreach (double x in arr)
+        {
+            System.Console.Write(x + " ");
+        }
+        System.Console.WriteLine();
+
+        double min = arr[0];
+        double max = arr[0];
+        foreach(double x in arr)
+        {
+            if (x < min)
+            {
+                min = x;
+            }
+            if (x > max)
+            {
+                max = x;
+            }
+        }
+
+        double diff = max - min;
+
+        System.Console.WriteLine("Разница между максимальным и минимальным элементами: " + diff);
+    }
+}
